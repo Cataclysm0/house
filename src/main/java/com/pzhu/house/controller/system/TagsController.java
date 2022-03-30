4169cf90d6746feace52e93e40a422b84a6ed6e7
wu <1159987262@qq.com>
2022-03-30 20:07:56 +0800

    backup

---
+++ src/main/java/com/pzhu/house/controller/system/TagsController.java
@@ -0,0 +1,18 @@
+package com.pzhu.house.controller.system;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 标签表 前端控制器
+ * </p>
+ *
+ * @author WuSJ
+ * @since 2022-03-30
+ */
+@RestController
+@RequestMapping("/tags")
+public class TagsController {
+
+}
